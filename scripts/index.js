1a75e871c35551277526de2e69335d118896a7d9
Sweet Bun <baklyakova@yandex.ru>
2024-04-10 12:43:51 +0300

    -

--- scripts/index.js
+++ scripts/index.js
@@ -8,22 +8,25 @@
 
 // @todo: Вывести карточки на страницу
 // Функция для создания карточки
-function cardAdd (name, link, deletCard) {
+
+function createCard(name, link) {
   const cardTemplate = document.querySelector('#card-template').content;
   const card = cardTemplate.cloneNode(true);
 
   card.querySelector('.card__image').src = link;
   card.querySelector('.card__title').textContent = name;
-  card.querySelector('.card__delete-button').addEventListener('click', (event) => deletCard(event));
+  card.querySelector('.card__image').alt = name;
+  card.querySelector('.card__delete-button').addEventListener('click', deletCard);
+
   return card;
-};
+}
 
 function deletCard() {
-const removeCard = event.target.closest('.card');
-removeCard.remove()
-};
+  const removeCard = event.target.closest('.card');
+  removeCard.remove();
+}
 
- const placesList = document.querySelector('.places__list');
-  initialCards.forEach(cardData => {
-   placesList.append(cardAdd(cardData.name, cardData.link, deletCard));
-  });
+const showList = document.querySelector('.places__list');
+initialCards.forEach(cardData => {
+  showList.append(createCard(cardData.name, cardData.link));
+});
